cbe2e4c6643270b90ada17aa6553158581613f0e
AleezaMirza <103499647+AleezaMirza@users.noreply.github.com>
2025-05-20 23:50:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ngs/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
